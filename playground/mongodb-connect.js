3068c324c144e730dab5b4ba87230f1a48aae14d
bitbucket.org <buntsem@gmail.com>
2018-08-22 18:24:33 +0530

    added update function

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -16,16 +16,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  // db.collection('User').insertOne({
-  //   name: 'Bunty',
-  //   age: 27,
-  //   location: "New Delhi"
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert user', err);
-  //   }
-  //   console.log(result.ops[0]._id.getTimestamp());
-  // });
+  db.collection('User').insertOne({
+    name: 'Bunty',
+    age: 27,
+    location: "New Delhi"
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+    console.log(result.ops[0]._id.getTimestamp());
+  });
 
   db.close();
 });
