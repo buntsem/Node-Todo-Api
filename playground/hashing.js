488e8819bc80405a2132fde980519fbaf9eda6bf
bitbucket.org <buntsem@gmail.com>
2018-09-02 17:16:33 +0530

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
-var data = {
-  id: 10
-}
+const bcrypt = require('bcryptjs');
 
 
-var token = jwt.sign(data, '123abc')
+var password = '123ac!'
 
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// })
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded',decoded);
+
+var hashedPasswod = '$2a$10$srsHhsfZos4ZPGoEAZFBJeJYeUFDKf4nb/z8bkz0zrNkV6mfymZ3i';
+bcrypt.compare(password, hashedPasswod, (err, res) => {
+  console.log(res);
+})
+// var data = {
+//   id: 10
+// }
+//
+//
+// var token = jwt.sign(data, '123abc')
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded',decoded);
 
 
 // var message = 'I am user number 3';
